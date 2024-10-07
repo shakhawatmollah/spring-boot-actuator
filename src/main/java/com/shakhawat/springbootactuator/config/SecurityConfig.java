722c770739c0924ef41043bc89c873b862f001b5
Shakhawat Mollah <sh.sumon99@gmail.com>
2024-10-07 12:34:33 +0600

    Spring security implement

--- src/main/java/com/shakhawat/springbootactuator/config/SecurityConfig.java
+++ src/main/java/com/shakhawat/springbootactuator/config/SecurityConfig.java
@@ -1,4 +1,50 @@
 package com.shakhawat.springbootactuator.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+@EnableWebSecurity
 public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests((requests) -> requests
+                        .requestMatchers("/mgt-details/**").hasRole("ADMIN") // Restrict access to actuator endpoints
+                        .anyRequest().authenticated()
+                )
+                .csrf(AbstractHttpConfigurer::disable) // Disable CSRF for non-browser clients
+                .httpBasic(withDefaults()); // Enable HTTP Basic authentication
+
+        return http.build();
+    }
+
+    @Bean
+    public UserDetailsService users() {
+
+        //{noop} mean that password is not encrypted. If you want to use encryption you must declare it. For example using {bcrypt}
+
+        UserDetails user1 = User.builder()
+                .username("admin")
+                .password("{noop}admin")
+                .roles("ADMIN")
+                .build();
+        UserDetails user2 = User.builder()
+                .username("user")
+                .password("{noop}user")
+                .roles("USER")
+                .build();
+        return new InMemoryUserDetailsManager(user1, user2);
+    }
 }
